9dc5e271540f3915a3f13c15902f3f8f4579fb7b
vishalhub <vishalweb9@gmail.com>
2022-08-25 02:43:27 +0530

    initial upload woth empty material app canvas for testing

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // const MyWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text("Hello world"),
+    );
+  }
+}
